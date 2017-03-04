0ba08271ddd5a30a5c01bb614dffd598841ce7b6
Tao Xie <xietao26@gmail.com>
2017-03-04 14:52:37 +0800

    Add login interface.

--- Inventory/PLL/Login.Designer.cs
+++ Inventory/PLL/Login.Designer.cs
@@ -28,48 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.profileDataGrid = new System.Windows.Forms.DataGridView();
-            this.loadProfile = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.profileDataGrid)).BeginInit();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.forgetLinkLabel = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // profileDataGrid
+            // usernameLabel
             // 
-            this.profileDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.profileDataGrid.Location = new System.Drawing.Point(98, 97);
-            this.profileDataGrid.Name = "profileDataGrid";
-            this.profileDataGrid.RowTemplate.Height = 23;
-            this.profileDataGrid.Size = new System.Drawing.Size(335, 174);
-            this.profileDataGrid.TabIndex = 0;
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.usernameLabel.Location = new System.Drawing.Point(48, 22);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(68, 12);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username:";
             // 
-            // loadProfile
+            // passwordLabel
             // 
-            this.loadProfile.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.loadProfile.Location = new System.Drawing.Point(333, 26);
-            this.loadProfile.Name = "loadProfile";
-            this.loadProfile.Size = new System.Drawing.Size(100, 44);
-            this.loadProfile.TabIndex = 1;
-            this.loadProfile.Text = "Load Profile";
-            this.loadProfile.UseVisualStyleBackColor = true;
-            this.loadProfile.Click += new System.EventHandler(this.loadProfile_Click);
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.passwordLabel.Location = new System.Drawing.Point(48, 59);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(68, 12);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.Location = new System.Drawing.Point(140, 19);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(200, 21);
+            this.usernameTextbox.TabIndex = 2;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(140, 56);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(200, 21);
+            this.passwordTextbox.TabIndex = 3;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(152, 93);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(253, 93);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // forgetLinkLabel
+            // 
+            this.forgetLinkLabel.AutoSize = true;
+            this.forgetLinkLabel.Location = new System.Drawing.Point(245, 134);
+            this.forgetLinkLabel.Name = "forgetLinkLabel";
+            this.forgetLinkLabel.Size = new System.Drawing.Size(95, 12);
+            this.forgetLinkLabel.TabIndex = 6;
+            this.forgetLinkLabel.TabStop = true;
+            this.forgetLinkLabel.Text = "Forget Password";
+            this.forgetLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.forgetLinkLabel_LinkClicked);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 313);
-            this.Controls.Add(this.loadProfile);
-            this.Controls.Add(this.profileDataGrid);
+            this.ClientSize = new System.Drawing.Size(416, 172);
+            this.Controls.Add(this.forgetLinkLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.usernameTextbox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
             this.Name = "Login";
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.profileDataGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView profileDataGrid;
-        private System.Windows.Forms.Button loadProfile;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.LinkLabel forgetLinkLabel;
     }
 }
--- Inventory/PLL/Login.cs
+++ Inventory/PLL/Login.cs
@@ -18,11 +18,62 @@ namespace Inventory.PLL
             InitializeComponent();
         }
 
-        private void loadProfile_Click(object sender, EventArgs e)
+        private void closeButton_Click(object sender, EventArgs e)
         {
-            ProfileBLL objBLL = new ProfileBLL();
-            profileDataGrid.DataSource = objBLL.GetProfile();
-            
+            DialogResult res = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(DialogResult.Yes == res)
+            {
+                Application.Exit();
+            }
+        }
+
+        // Ask the same question when the cross button is clicked
+        protected override bool ProcessCmdKey(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        {
+            try
+            {
+                if(msg.WParam.ToInt32() == (int)Keys.Escape)
+                {
+                    DialogResult res = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (DialogResult.Yes == res)
+                    {
+                        Application.Exit();
+                    }
+                }
+                else
+                {
+                    return base.ProcessCmdKey(ref msg, keyData);
+                }
+            }
+            catch
+            {
+
+                MessageBox.Show("Key Press Error..!");
+            }
+
+            return false;  
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if("Admin" == usernameTextbox.Text && "Password" == passwordTextbox.Text)
+            {
+                Home.HomeInventory home = new Home.HomeInventory();
+                home.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Please check your username and password.");
+            }
+        }
+
+        private void forgetLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Login ss = new Login();
+            ss.Show(); 
         }
     }
 }
